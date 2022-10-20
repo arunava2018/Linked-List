370a65652f64b1b8eb2e269edd8c500702b49e4c
Arunava Banerjee <100664086+arunava2018@users.noreply.github.com>
2022-10-20 11:15:47 +0530

    Add files via upload

---
+++ LinkedListLength.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+ 
+void linkedListTraversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int getlength(struct Node *head){
+    int count=0;
+    while(head!=NULL){
+        head=head->next;
+        count++;
+    }
+    return count;
+}
+ 
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+ 
+    // Allocate memory for nodes in the linked list in Heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+ 
+    // Link first and second nodes
+    head->data = 7;
+    head->next = second;
+ 
+    // Link second and third nodes
+    second->data = 11;
+    second->next = third;
+ 
+    // Link third and fourth nodes
+    third->data = 41;
+    third->next = fourth;
+ 
+    // Terminate the list at the third node
+    fourth->data = 66;
+    fourth->next = NULL;
+ 
+   // linkedListTraversal(head);
+    int length=getlength(head);
+    printf("The length of the linked list is %d",length);
+    return 0;
+}
